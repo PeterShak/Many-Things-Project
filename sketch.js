69e3c9c58a2a9453947289437be1906d0a372025
PeterShak <peter_shaknes@avenues-ny.org>
2024-12-08 23:58:56 -0500

    Made many balls by reworking original code

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
 let paddle;
-let ball;
+let balls = [];
 
 function setup() {
   createCanvas(400, 400);
   
+  for (let i = 0; i < 10; i++) {
+    balls.push(createBall()); // Push a new ball into the balls array
+  }
+
+  
   paddle = {
     x: 50,
     y: 350,
-    w: 70,
-    h: 10,
+    w: 2500,
+    h: 30,
     
     show: function() {
       fill(0);
@@ -20,59 +25,67 @@ function setup() {
       this.x = mouseX - this.w / 2; // offsets it to be centered on the       mouse
     }
   };
-  
-  ball = {
-    x: random(255), 
-    y: random(255),
-    radius: 20,
-    speedX: random(1,3),
-    speedY: random(1,3),
-    
-    show: function() {
-      fill(0, 255, 0);
-      noStroke();
-      ellipse(this.x, this.y, this.radius * 2, this.radius * 2); 
-    },
-    
-    update: function() {
-      this.x += this.speedX;
-      this.y += this.speedY;
-      
-      if(this.x - this.radius <= 0 || this.x + this.radius >= width) {
-        this.speedX *= -1
-      }
-      
-      if(this.y - this.radius <= 0) {
-        this.speedY *= -1
-      }
-      if (this.y + this.radius >= height) {
-        this.speedY *= -1;  
-        this.y = height - this.radius;  
-        noLoop();
-      }
-    }
-  }
 }
-
 function draw() {
   background(220);
   paddle.update();
   paddle.show();
-  ball.show();
-  ball.update();
   
-  if (ball.y + ball.radius >= paddle.y && ball.y + ball.radius <= paddle.y + paddle.h && ball.x >= paddle.x && ball.x <= paddle.x + paddle.w) {
-    ball.speedY *= -1;  
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].update();
+    balls[i].show();
+
+    if (balls[i].y + balls[i].radius >= paddle.y && balls[i].y + balls[i].radius <= paddle.y + paddle.h && balls[i].x >= paddle.x && balls[i].x <= paddle.x + paddle.w) {
+      balls[i].speedY *= -1;
+    }
   }
+}
+
+function createBall() {
+  return {
+    x: random(width),
+    y: random(height / 2),  
+    radius: 20,
+    speedX: random(1, 3),
+    speedY: random(1, 3),
+    
+    show: function() {
+      fill(0, 255, 0);
+      noStroke();
+      ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+    },
+    
+    update: function() {
+      
+
+      this.x += this.speedX;
+      this.y += this.speedY;
+      
+      if (this.x - this.radius <= 0 || this.x + this.radius >= width) {
+        this.speedX *= -1;
+      }
+      
+      if(this.y - this.radius <= 0) {
+        this.speedY *= -1;
+      }
+      
+      if (this.y + this.radius >= height) {
+        this.speedY *= -1;  
+        this.y = height - this.radius;  
+        
+        noLoop();  
+      }
+    }
+  };
 }
 
 function keyPressed() {
   if (keyCode === ENTER) {
     
-    ball.x = random(255);
-    ball.y = random(255);
-    ball.speedX = 2;
-    ball.speedY = 2;
+   balls = []; 
+    for (let i = 0; i < 10; i++) {
+      balls.push(createBall()); 
+    }
 
     loop();
   }
